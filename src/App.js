668c1166d5208aa44fe4c3fb4d651fb3390e5c49
ItsMuskanGoyal <user@Muskan-MacBook-Pro.local>
2024-01-21 22:22:48 +0530

    basic Structure done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import ContactMe from './pages/ContactMe';
+import Experience from './pages/Experience';
+import Home from './pages/Home';
+import Notes from './pages/Notes';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <Home/>
+      <Projects/>
+      <Notes/>
+      <Experience/>
+      <ContactMe/>
+      <Footer/>
     </div>
   );
 }
